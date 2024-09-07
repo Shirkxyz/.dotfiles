7400b8b428ccd4c4e7bc7a66b37d2ccf1ce808ee
Shirkxyz <shirkxyz@proton.me>
2024-09-07 00:08:57 -0400

    fish fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,7 +16,6 @@ if status is-interactive
     if type -q eza
       alias ll "eza -l --icons"
       alias lla "eza -A -l --icons"
-      alias lg "lazygit"
       alias ff "fastfetch -l  ~/.config/fastfetch/pngs/*.png --logo-width 28 --logo-height 12"
     end
 
